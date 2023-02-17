1258546369bcb9d071d215a38d1f051bf059c168
mehmetAltinpinar <mehmetu7@gmail.com>
2023-02-17 11:21:04 -0500

    On the Commit tab I wrote: * code changed in the existing class GitHubProject02 * a new class called "GitHubPractice03" was added

--- src/test/java/Mentoring_20230216/GitHubPractice02.java
+++ src/test/java/Mentoring_20230216/GitHubPractice02.java
@@ -2,6 +2,6 @@ package Mentoring_20230216;
 
 public class GitHubPractice02 {
     public static void main(String[] args) {
-        System.out.println("Hello GitHubPractice02");
+        System.out.println("Hello GitHub Practice 02");
     }
 }
